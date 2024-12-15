5188960cac18ed4c33d41a28a9c28c1f29af853f
Andrii Stadnyk <stadnyk.andy@gmail.com>
2024-12-15 02:17:43 +0200

    vercell url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -38,8 +38,8 @@ export function formatPhoneNumber(phoneNumber: string) {
 }
 
 export const getBaseUrl = () => {
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   }
 
   return `http://localhost:3000`;
