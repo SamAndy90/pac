2543b37c098bad598ea002c2d226e7bd6dd75229
Andrii Stadnyk <stadnyk.andy@gmail.com>
2025-01-12 01:50:38 +0200

    new features 2

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -8,17 +8,17 @@ export default function StudioPage() {
   const { user } = useUser();
   const userRole = user?.organizationMemberships[0]?.role;
 
-  // if (userRole == "org:admin") {
-  return (
-    <div className="pt-24 pb-16 bg-pka_background">
-      <NextStudio config={config} />
-    </div>
-  );
-  // } else {
-  //   return (
-  //     <div className="pt-24 text-center text-pka_blue bg-pka_background">
-  //       <p>No access to the studio.</p>
-  //     </div>
-  //   );
-  // }
+  if (userRole == "org:admin") {
+    return (
+      <div className="pt-24 pb-16 bg-pka_background">
+        <NextStudio config={config} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="pt-24 text-center text-pka_blue bg-pka_background">
+        <p>No access to the studio.</p>
+      </div>
+    );
+  }
 }
